41841e1ebd22e6bf4d88791a9a878602766c951a
eldersb <122701368+eldersb@users.noreply.github.com>
2024-04-23 12:05:22 -0300

    Fazendo requisição de motos

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
 const app = () => {
     return {
-        motos: [],
+        fabricante: '',
+        fabricantes: [],
+        fabricanteSelecionado: null,
+        modelos: [],
+
 
         init() {
             axios.get('https://fipe.parallelum.com.br/api/v2/motorcycles/brands')
                 .then((response) => {
-                    this.motos = response.data;
+                    this.fabricantes = response.data;
                     console.log(this.motos);
                 })
                 .catch(function (error) {
                     console.log(error);
                 })
+        },
+        selecionarFabricante(){
+           
+            
+            const url = `https://fipe.parallelum.com.br/api/v2/motorcycles/brands/${this.fabricante}/models`
+            
+
+            axios.get(url)
+             .then(response =>{
+                this.modelos = response.data;
+                console.log(modelo.id)
+             })
+             .catch(function(error){
+                console.log(error);
+             })
+            
         }
 
     }
