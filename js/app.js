ea7448dc33fbdc854c13547d0f53cf9ac5edf74a
eldersb <122701368+eldersb@users.noreply.github.com>
2024-04-23 13:48:57 -0300

    Requisição modelo e ano moto

--- js/app.js
+++ js/app.js
@@ -1,36 +1,57 @@
 const app = () => {
     return {
-        fabricante: '',
+         fabricante: '',
+         modelo: '',
         fabricantes: [],
-        fabricanteSelecionado: null,
+        fabricanteSelecionado: [],
         modelos: [],
+        anos: [],
 
 
         init() {
             axios.get('https://fipe.parallelum.com.br/api/v2/motorcycles/brands')
                 .then((response) => {
                     this.fabricantes = response.data;
-                    console.log(this.motos);
+                    console.log(this.fabricante);
                 })
                 .catch(function (error) {
                     console.log(error);
                 })
         },
         selecionarFabricante(){
-           
+            // console.log(this.fabricante)
+            // console.log(this.fabricantes)
+            // console.log(this.fabricanteSelecionado)
+            // console.log(this.modelos)
             
             const url = `https://fipe.parallelum.com.br/api/v2/motorcycles/brands/${this.fabricante}/models`
-            
 
+            
+            this.modelos = [];
             axios.get(url)
              .then(response =>{
                 this.modelos = response.data;
-                console.log(modelo.id)
+                console.log(response.data);
+                
              })
              .catch(function(error){
                 console.log(error);
              })
             
+        },
+        selecionarModelo(){
+            const url = `https://fipe.parallelum.com.br/api/v2/motorcycles/brands/${this.fabricante}/models/${this.modelo}/years`
+
+            this.anos = [];
+            axios.get(url)
+             .then(response =>{
+                this.anos = response.data;
+                console.log(response.data);
+                
+             })
+             .catch(function(error){
+                console.log(error);
+             })
         }
 
     }
